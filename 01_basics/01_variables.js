d8bfce893df8db98f73c619645e906121ac8e576
burhanfaheem3697 <152875094+burhanfaheem3697@users.noreply.github.com>
2024-05-12 19:37:07 +0000

    variablefile

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 324234;
+let accountemail = 'hotmail@gmail.com';
+var accountpasswrd = '398234'
+accountcity = 'bangalore';
+// prefer not to use var because of issue in block and functional scope
+console.table([accountId,accountemail,accountpasswrd,accountcity]);
